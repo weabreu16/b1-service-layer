fd658a959b89e4871bfdb10890e5fef1e6c28909
wabreufmp <wabreu@fmp.com.do>
2023-08-02 13:03:19 -0400

    feat(queries): add SAPQueryable
    execute LinQ's queryable operations using SAP Root Expression as base

---
+++ src/B1ServiceLayer/Queries/SAPQueryable.cs
@@ -0,0 +1,32 @@
+﻿using B1ServiceLayer.Expressions;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace B1ServiceLayer.Queries;
+
+public class SAPQueryable<TSAPEntity>: IQueryable<TSAPEntity>, IOrderedQueryable<TSAPEntity>
+{
+    public Type ElementType => typeof(TSAPEntity);
+
+    public Expression Expression { get; }
+
+    public IQueryProvider Provider { get; }
+
+    public SAPQueryable(IQueryProvider provider, string resourceName)
+    {
+        Provider = provider;
+        Expression = new SAPRootExpression(ElementType, resourceName);
+    }
+
+    public SAPQueryable(IQueryProvider provider, Expression expression)
+    {
+        Provider = provider;
+        Expression = expression;
+    }
+
+    public IEnumerator<TSAPEntity> GetEnumerator()
+        => Provider.Execute<IEnumerable<TSAPEntity>>(Expression).GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => Provider.Execute<IEnumerable>(Expression).GetEnumerator();
+}
