d4a58272578cf97f4f52cf7da66ba42f8cd9ebc9
wabreufmp <wabreu@fmp.com.do>
2023-08-02 12:27:44 -0400

    feat(attributes): sap entity attribute
    attribute to set the SAP's resource name of target class

---
+++ src/B1ServiceLayer/Attributes/SAPEntityAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace B1ServiceLayer.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class SAPEntityAttribute : Attribute
+{
+    public string ResourceName { get; set; }
+
+    public SAPEntityAttribute(string resourceName)
+    {
+        ResourceName = resourceName;
+    }
+}
