89c0633d81694ced4543b9372f8659b2f3d63bb5
wabreufmp <wabreu@fmp.com.do>
2023-08-02 09:34:40 -0400

    chore: add async query provider interface

---
+++ src/B1ServiceLayer/Interfaces/IAsyncQueryProvider.cs
@@ -0,0 +1,8 @@
+﻿using System.Linq.Expressions;
+
+namespace B1ServiceLayer.Interfaces;
+
+internal interface IAsyncQueryProvider: IQueryProvider
+{
+    Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken = default);
+}
