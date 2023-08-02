6b3a05fa29c2a43fa68ed0e0c36a4de3c8e93a22
wabreufmp <wabreu@fmp.com.do>
2023-08-02 12:47:07 -0400

    feat(visitors): add sap join where expression visitor
    translate where expression from cross join operation to SAP query filter operation

---
+++ src/B1ServiceLayer/Visitors/SAPJoinWhereExpressionVisitor.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+
+namespace B1ServiceLayer.Visitors;
+
+public class SAPJoinWhereExpressionVisitor : SAPWhereExpressionVisitor
+{
+    private readonly Dictionary<Type, string> _resources;
+
+    public SAPJoinWhereExpressionVisitor(Dictionary<Type, string> resources) : base()
+    {
+        _resources = resources;
+    }
+
+    protected override Expression VisitMember(MemberExpression node)
+    {
+        Append($"{_resources![node.Member.DeclaringType!]}/{node.Member.Name}");
+
+        return node;
+    }
+}
